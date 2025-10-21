d5e358b35457011ab3e65b8d22951b7e199a8312
Tania Kolesnik <kolesnik@gmail.com>
2025-10-21 22:33:48 +0100

    add solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    params = command.split()
+
+    if len(params) < 3:
+        return
+
+    if params[0] != "cp":
+        return
+
+    if not os.path.exists(params[1]):
+        return
+
+    if params[1] != params[2]:
+        with open(params[1], "r") as file_in, open(params[2], "w") as file_out:
+            for line in file_in:
+                file_out.write(line)
